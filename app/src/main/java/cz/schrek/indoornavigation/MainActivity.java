79a518612fcfed8738a93288a3ed84da2c4aa500
Ondřej Schrek <ondrej.schrek@gmail.com>
2016-08-23 09:38:03 +0200

    refaktor

--- app/src/main/java/cz/schrek/indoornavigation/MainActivity.java
+++ app/src/main/java/cz/schrek/indoornavigation/MainActivity.java
@@ -1,70 +1,48 @@
 package cz.schrek.indoornavigation;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.PointF;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import com.davemorrissey.labs.subscaleview.ImageSource;
-
-import java.util.Random;
+import android.widget.ImageView;
+import com.qozix.tileview.TileView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button rotateBut;
-    private Button pointBut;
-    private IndoorMap indoorMap;
-    private TextView label;
+    private Button centerBut;
+    private TileView tile;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        indoorMap = (IndoorMap) findViewById(R.id.imageView);
-        rotateBut = (Button) findViewById(R.id.rotateBut);
-        pointBut = (Button) findViewById(R.id.pointBut);
-        label = (TextView) findViewById(R.id.label);
+
+        centerBut = (Button) findViewById(R.id.center);
+        tile = (TileView) findViewById(R.id.tile);
 
 
-        indoorMap.setImage(ImageSource.resource(R.drawable.map));
-        indoorMap.setPadding(20, 0, 20, 0);
-        indoorMap.setZoomEnabled(false);
-
-        DistanceConverter dc = new DistanceConverter(this);
-        float[] dim;
-        dim = dc.cmToPx(0, 0);
-
-        indoorMap.setPosition(new PositionPoint(dim[0], dim[1], this));
+        tile.setSize(827, 486);
+        tile.addDetailLevel(1f, "tile-%d_%d.png");
+        tile.setShouldRenderWhilePanning(true);
 
 
-        rotateBut.setOnClickListener(new View.OnClickListener() {
+        centerBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                indoorMap.setRotation(indoorMap.getRotation() + 10);
+                tile.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView img = new ImageView(getApplicationContext());
+                        img.setImageResource(R.drawable.pushpin_blue);
+//                        double x =
+                        tile.addMarker(img, 0, 0, null, null);
+                    }
+                });
             }
         });
 
-        pointBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DistanceConverter dc = new DistanceConverter(MainActivity.this);
-                float[] dim;
-                Random rnd = new Random();
-                float x = rnd.nextInt(700);
-                float y = rnd.nextInt(400);
-
-                dim = dc.cmToPx(350, 200);
-                PositionPoint point = new PositionPoint(dim[0], dim[1], MainActivity.this);
-                indoorMap.setPosition(point);
-                label.setText(point.toString() + "\n");
-                label.append(x + "   " + y);
-            }
-        });
 
     }
 }
